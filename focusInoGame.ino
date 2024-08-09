574affd5f4c3ecff261fdb214ec7624a8485e63f
Atshmuel <98228213+Atshmuel@users.noreply.github.com>
2024-08-09 21:30:37 +0300

    vars and arrays
    created all the vars the will be used in the project

--- focusInoGame.ino
+++ focusInoGame.ino
@@ -1,3 +1,42 @@
+#define ARR_LEN 4
+#define MAX_TIMES 3
+
+#define greenLed 10
+#define yellowLed 9
+#define redLed 11
+#define blueLed 6
+
+#define greenBtn 2
+#define yellowBtn 3
+#define redBtn 4
+#define blueBtn 12
+
+#define buzzerPin 5
+
+const int timeBuffer = 150;
+const int maxDiff = 1000;
+
+unsigned long startTime;
+unsigned long endTime;
+
+int currState;
+int currectPress = 0;
+int pressCounter = 0;
+
+bool initProgram = true;
+bool isWon;
+bool restart=false;
+
+const int btnsArr[ARR_LEN] = { greenBtn, redBtn, yellowBtn, blueBtn };
+const int ledsArr[ARR_LEN] = { greenLed, redLed, yellowLed, blueLed };
+const int tonesArr[ARR_LEN] = { NOTE_C4, NOTE_G3, NOTE_A3, NOTE_F2 };
+
+int currValArr[ARR_LEN];
+int lastValArr[ARR_LEN];
+unsigned long lastPressTime[ARR_LEN];
+int chosenIndexsArr[MAX_TIMES];
+int pressedIndexs[MAX_TIMES];
+
 void setup() {
   // put your setup code here, to run once:
 
